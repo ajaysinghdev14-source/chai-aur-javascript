0cf2b375a5f80d7a5d63434bf41af9795098bcbf
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-07-15 07:36:03 +0000

    Add foundational Node.js script with global object type checks

---
+++ node-projects/03_node/01_node_foundation.js
@@ -0,0 +1,12 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const os = require("node:os");
+
+// console.log("NODEJS: ", process.versions.node);
+// console.log("V8: ", process.versions.v8);
+// console.log("Libuv: ", process.versions.uv);
+// console.log("Plateform: ", process.platform);
+// console.log("CPU: ", os.cpus().length);
+
+console.log(typeof global);
+console.log(typeof globalThis);
